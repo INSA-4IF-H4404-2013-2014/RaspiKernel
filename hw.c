36f0e7b5c6d54347a57823417b6425140dc7dda8
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-10-14 23:15:08 +0200

    Adds comments on the interuption mechanism (hw.c)

--- hw.c
+++ hw.c
@@ -1,17 +1,30 @@
 #include "hw.h"
 
-#define CS 0x20003000
-#define CLO 0x20003004
-#define C0 0x2000300C
-#define C1 0x20003010
-#define C2 0x20003014
-#define C3 0x20003018
+// doc1 : doc_arm_peripherals.pdf
+
+// Why not 0x7E003000 ? (doc1:p172)
+#define CS 0x20003000  // System Timer Control/Status (doc1:p172)
+#define CLO 0x20003004 // System Timer Counter Lower 32 bits (doc1:p172)
+#define C0 0x2000300C  // System Timer Compare 0
+#define C1 0x20003010  // System Timer Compare 1
+#define C2 0x20003014  // System Timer Compare 2
+#define C3 0x20003018  // System Timer Compare 3
 
 #define GPFSEL1 0x20200004
 #define GPSET0  0x2020001C
 #define GPCLR0  0x20200028
 
-#define INTERVAL 0x00080000
+// 0x218 Enable Basic IRQs
+
+
+
+/*
+ * (doc1:p114)
+ * BIT  0 : ARM Timer IRQ pending
+ */
+#define INTERVAL 0x2000B200 // (doc1:p112)
+
+
 
 extern void PUT32 ( unsigned int, unsigned int );
 extern unsigned int GET32 ( unsigned int );
@@ -87,5 +100,15 @@ init_hw()
     enable_timer_irq();
 
     /* Enable interrupt *line* */
-    PUT32(0x2000B210, 0x00000002);
+    /* (doc1:p116):
+     * 0x2000B210 = Enable IRQs 1 (doc1:p116)
+     * 0x2000B214 = Enable IRQs 2
+     * 0x2000B218 = Enable Basics IRQs
+     * 0x2000B21C = Disable IRQs 1
+     * 0x2000B220 = Disable IRQs 2
+     * 0x2000B224 = Disable Basics IRQs
+     *
+     * Diference betwen {Enable,Disable} IRGs 1 ??????????g
+     */
+    PUT32(0x2000B210, 0x00000002); // why not 0x7E00B000 ?
 }
