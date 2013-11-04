0f5e44baaea3416926e69dea18bc6fe81ae43863
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-04 22:04:14 +0100

    Separates processes out of the kernel main file

--- os/kernel_main.c
+++ os/kernel_main.c
@@ -3,39 +3,9 @@
 #include "hw.h"
 
 
-
-//------------------------------------------------------------------------
-
 void
 main_process(void);
 
-
-//------------------------------------------------------------------------
-
-void
-ping(uint32_t * args)
-{
-    uint32_t cpt = *args;
-
-    while (1) {
-        cpt++;
-    }
-}
-
-void
-pong(uint32_t * args)
-{
-    uint32_t cpt = *args;
-    uint32_t i;
-
-    for (i = 0; i < 16; i++)
-    {
-        cpt *= cpt;
-    }
-}
-
-
-//------------------------------------------------------------------------
 int
 kernel_main(void)
 {
@@ -45,16 +15,6 @@ kernel_main(void)
     // create the main process
     uint32_t main_process_PID = process_create((process_func_t) main_process, 0);
     process_start(main_process_PID);
-
-    // create kernel processes
-    uint32_t ping_start = 17;
-    uint32_t pong_start = 33;
-
-    uint32_t ping_pid = process_create((process_func_t)ping, &ping_start);
-    uint32_t pong_pid = process_create((process_func_t)pong, &pong_start);
-
-    process_start(ping_pid);
-    process_start(pong_pid);
 
     init_hw();
 
