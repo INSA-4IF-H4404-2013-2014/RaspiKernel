c6dc1543f3b46e0558ae87eaf17812b87c58bcf2
Guillaume Abadie <guillaume.abadie@gmail.com>
2013-11-07 18:26:59 +0100

    Fixes #4151: Adds exemple in main.c using semaphores workinf perfectly

--- main.c
+++ main.c
@@ -1,33 +1,40 @@
 
 #include "os/api_process.h"
+#include "os/api_sync_sem.h"
 
 
 static void
-sub_process(uint32_t * p_i)
+sub_process(sync_sem_t * sem_array)
 {
-    uint32_t i = *p_i;
+    uint32_t i = 1;
 
     for ( ; ; )
     {
+        sync_sem_wait(sem_array + 0);
         i += 2;
+        sync_sem_post(sem_array + 1, 1);
     }
 }
 
 void
 main_process(void)
 {
-    uint32_t i = 33;
-    uint32_t j = 10;
+    sync_sem_t sem_array[2];
 
-    uint32_t PID = process_create((process_func_t) sub_process, &j);
+    sync_sem_init(&sem_array[0], 0);
+    sync_sem_init(&sem_array[1], 0);
+
+    uint32_t i = 0;
+
+    uint32_t PID = process_create((process_func_t) sub_process, sem_array);
 
     process_start(PID);
 
-    process_pause(process_get_pid());
-
     for ( ; ; )
     {
-        i += 3;
+        i += 2;
+        sync_sem_post(sem_array + 0, 1);
+        sync_sem_wait(sem_array + 1);
     }
 }
 
