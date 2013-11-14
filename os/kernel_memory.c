ebe4246b605f3802571125ac07ddc4dc7e850ca9
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-11-14 11:37:36 +0100

    Begins #4169 kernel memory allocator

---
+++ os/kernel_memory.c
@@ -0,0 +1,11 @@
+#include "kernel_memory.h"
+
+void * kernel_allocate_memory()
+{
+	return (void *)0;
+}
+
+void * kernel_deallocate_memory()
+{
+	return (void *)0;
+}
---
+++ os/kernel_memory.h
@@ -0,0 +1,7 @@
+#ifndef _H_KERNEL_MEMORY
+#define _H_KERNEL_MEMORY
+
+void * kernel_allocate_memory();
+void * kernel_deallocate_memory();
+
+#endif
